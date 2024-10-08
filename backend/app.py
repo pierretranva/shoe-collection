98631d1dc561e678b20f2c89e46928c8934b0b99
mrvalf <mrvalf@gmail.com>
2024-10-08 13:27:40 -0400

    Get (all, by id and by name), delete (by id), update and create for User table. Get all and by id for Post and Shoe tables. Also deleted the databases and added them to gitignore.

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,114 @@
-import sqlite3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from database import (
+    get_all_users, get_user_by_id, get_user_by_name, delete_user_by_id, update_user_by_id, add_user_to_database,
+    get_all_posts, get_post_by_id,
+    get_all_shoes, get_shoe_by_id
+)
+import uvicorn
 
 app = FastAPI()
+
+
+class UpdateUserPayload(BaseModel):
+    user_id: int
+    name: str
+    date_of_birth: str
+    hometown: str
+
+
+class CreateUserPayload(BaseModel):
+    name: str
+    password: str
+    date_of_birth: str
+    hometown: str
+
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
 
+@app.get('/users')
+def get_users():
+    users = get_all_users()
+    return users
 
 
+@app.get('/users/{user_id}')
+def get_user(user_id: int):
+    user = get_user_by_id(user_id)
+    if user:
+        return user
+    else:
+        raise HTTPException(status_code=404, detail='User not found')
 
+
+@app.delete('/users/{user_id}')
+def delete_user(user_id: int):
+    deleted = delete_user_by_id(user_id)
+    if deleted:
+        return {'message': 'User deleted successfully'}
+    else:
+        raise HTTPException(status_code=404, detail='User not found')
+
+
+@app.put('/update_user')
+def update_user(payload: UpdateUserPayload):
+    updated = update_user_by_id(payload.user_id, payload.name, payload.date_of_birth, payload.hometown)
+    if updated:
+        return {'message': 'User updated successfully'}
+    else:
+        raise HTTPException(status_code=404, detail='User not found')
+
+
+@app.put('/add_user')
+def add_user(payload: CreateUserPayload):
+    updated = add_user_to_database(payload.name, payload.password, payload.date_of_birth, payload.hometown)
+    if updated:
+        return {'message': 'User created successfully'}
+    else:
+        raise HTTPException(status_code=404, detail='User not created')
+
+
+@app.get('/users/name/{name}')
+def get_users_by_name(name: str):
+    users = get_user_by_name(name)
+    if users:
+        return users
+    else:
+        raise HTTPException(status_code=404, detail='No users found with the given name')
+
+
+@app.get('/posts')
+def get_posts():
+    posts = get_all_posts()
+    return posts
+
+
+@app.get('/posts/{post_id}')
+def get_post(post_id: int):
+    post = get_post_by_id(post_id)
+    if post:
+        return post
+    else:
+        raise HTTPException(status_code=404, detail='Post not found')
+
+
+@app.get('/shoes')
+def get_shoes():
+    shoes = get_all_shoes()
+    return shoes
+
+
+@app.get('/shoes/{shoe_id}')
+def get_shoe(shoe_id: int):
+    shoe = get_shoe_by_id(shoe_id)
+    if shoe:
+        return shoe
+    else:
+        raise HTTPException(status_code=404, detail='Shoe not found')
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=False)
--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,14 @@ import sqlite3
 import bcrypt
 import random
 import string
+
 con = sqlite3.connect("database.db")
 
 cur = con.cursor()
 
+
 def init_db():
-    #create all the tables if they don't exists
+    # create all the tables if they don't exists
 
     sql_create_user = """CREATE TABLE IF NOT EXISTS User (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -87,7 +89,6 @@ def init_db():
     FOREIGN KEY (post_id) REFERENCES Post(post_id))"""
     # sql_create_comment = " ".join(line.strip() for line in sql_create_comment.splitlines())
 
-
     cur.execute(sql_create_user)
     cur.execute(sql_create_post)
     cur.execute(sql_create_shoe)
@@ -104,31 +105,147 @@ def init_db():
 def hash_password(password):
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def check_password(input_password, stored_hashed_password):
     return bcrpyy.checkpw(input_password.encode('utf-8'), stored_hashed_password)
 
+
 def create_user(username, password, date_of_birth, hometown):
-    password = hash(password)
-    cur.execute("INSERT INTO User (username, password, date_of_birth, hometown) VALUES (?, ?, ?, ?)", (username, password, date_of_birth, hometown))
+    password = hash_password(password)
+    cur.execute("INSERT INTO User (username, password, date_of_birth, hometown) VALUES (?, ?, ?, ?)",
+                (username, password, date_of_birth, hometown))
     con.commit()
+
+
 def create_admin(username, password):
-    password = hash(password)
+    password = hash_password(password)
     cur.execute("INSERT INTO Admin (username, password) VALUES (?, ?)", (username, password))
     con.commit()
 
 
-
 def populate_db():
     create_admin("admin", "admin")
-    #create 30 users
-    for i in range(0,29):
-        create_user("test"+str(i), ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)), "1999-01-0" + str(i), "New York")
-    #create 30 shoes
-    for i in range(0,29):
-        cur.execute("INSERT INTO Shoe (brand, model, year, color, creator_id) VALUES (?, ?, ?, ?, ?)", ("brand", "model" + str(i), 2021, "color", 1))
-    #create 30 posts
-    for i in range(0,29):
-        cur.execute("INSERT INTO Post (caption, picture_url, is_selling, price, selling_link, date, creator_id, related_shoe_id, admin_moderator_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", ("caption", "picture_url", 1, 100, "selling_link", "2021-01-0" + str(i), 1, 1, 1))
+    # create 30 users
+    for i in range(0, 29):
+        create_user("test" + str(i), ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)),
+                    "1999-01-0" + str(i), "New York")
+    # create 30 shoes
+    for i in range(0, 29):
+        cur.execute("INSERT INTO Shoe (brand, model, year, color, creator_id) VALUES (?, ?, ?, ?, ?)",
+                    ("brand", "model" + str(i), 2021, "color", 1))
+    # create 30 posts
+    for i in range(0, 29):
+        cur.execute(
+            "INSERT INTO Post (caption, picture_url, is_selling, price, selling_link, date, creator_id, related_shoe_id, admin_moderator_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            ("caption", "picture_url", 1, 100, "selling_link", "2021-01-0" + str(i), 1, 1, 1))
     con.commit()
-init_db()
-populate_db()
+
+
+# cursor.execute("SELECT * FROM User")
+try:
+    cur.execute("SELECT * FROM User")
+except:
+    init_db()
+    populate_db()
+
+
+def get_all_users():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM User")
+    users = [dict(user_id=row[0], name=row[1], date_of_birth=row[3]) for row in cursor.fetchall()]
+    connection.close()
+    return users
+
+
+def get_user_by_id(user_id):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM User WHERE user_id = ?", (user_id,))
+    row = cursor.fetchone()
+    connection.close()
+    if row:
+        return dict(user_id=row[0], name=row[1], date_of_birth=row[3])
+    return None
+
+
+def delete_user_by_id(user_id):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM User WHERE user_id = ?", (user_id,))
+    connection.commit()
+    deleted = cursor.rowcount > 0
+    connection.close()
+    return deleted
+
+
+def update_user_by_id(user_id, name, date_of_birth, home_town):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("UPDATE User SET username = ?, date_of_birth = ?, hometown = ? WHERE user_id = ?",
+                   (name, date_of_birth, home_town, user_id))
+    connection.commit()
+    updated = cursor.rowcount > 0
+    connection.close()
+    return updated
+
+
+def add_user_to_database(username, password, date_of_birth, hometown):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    password = hash_password(password)
+    cursor.execute("INSERT INTO User (username, password, date_of_birth, hometown) VALUES (?, ?, ?, ?)",
+                   (username, password, date_of_birth, hometown))
+    connection.commit()
+    updated = cursor.rowcount > 0
+    connection.close()
+    return updated
+
+
+def get_user_by_name(name):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM User WHERE username LIKE ?", (f"%{name}%",))
+    users = [dict(user_id=row[0], name=row[1], date_of_birth=row[3]) for row in cursor.fetchall()]
+    connection.close()
+    return users
+
+
+def get_all_posts():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Post")
+    posts = [dict(post_id=row[0], title=row[1], content=row[2]) for row in cursor.fetchall()]
+    connection.close()
+    return posts
+
+
+def get_post_by_id(post_id):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Post WHERE post_id = ?", (post_id,))
+    row = cursor.fetchone()
+    connection.close()
+    if row:
+        return dict(post_id=row[0], title=row[1], content=row[2])
+    return None
+
+
+def get_all_shoes():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Shoe")
+    shoes = [dict(shoe_id=row[0], brand=row[1], model=row[2]) for row in cursor.fetchall()]
+    connection.close()
+    return shoes
+
+
+def get_shoe_by_id(shoe_id):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Shoe WHERE shoe_id = ?", (shoe_id,))
+    row = cursor.fetchone()
+    connection.close()
+    if row:
+        return dict(shoe_id=row[0], brand=row[1], model=row[2])
+    return None
